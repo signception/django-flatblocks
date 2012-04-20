c408723a878139de97fefbccf3f5c984b2b749f2
Nate Thelen <nate@natethelen.com>
2012-04-20 11:13:03 -0700

    Fixed problem when I merged from my upstream

---
+++ flatblocks/migrations/0003_auto__del_unique_flatblock_slug.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Removing unique constraint on 'FlatBlock', fields ['slug']
+        db.delete_unique('flatblocks_flatblock', ['slug'])
+
+    def backwards(self, orm):
+        # Adding unique constraint on 'FlatBlock', fields ['slug']
+        db.create_unique('flatblocks_flatblock', ['slug'])
+
+    models = {
+        'flatblocks.flatblock': {
+            'Meta': {'unique_together': "(('slug', 'lang_code', 'site'),)", 'object_name': 'FlatBlock'},
+            'content': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'header': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'lang_code': ('django.db.models.fields.CharField', [], {'default': "'en'", 'max_length': '5', 'blank': 'True'}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['sites.Site']", 'null': 'True', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'sites.site': {
+            'Meta': {'ordering': "('domain',)", 'object_name': 'Site', 'db_table': "'django_site'"},
+            'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['flatblocks']
